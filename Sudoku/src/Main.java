06f715f91df0432ccf37cb860e9e3ae3a1165d8e
LouisonParant <louison.parant@etu.univ-lyon1.fr>
2025-01-29 10:55:26 +0100

    detect-os

--- Sudoku/src/Main.java
+++ Sudoku/src/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         boolean hard = false;
         int taille = 3;
-        Sudoku s = Parser.parseNXN("Sudoku\\src\\resources\\" + taille + "x" + taille + (hard ? "hard" : "") + ".csv");
+        Sudoku s;
+        if(System.getProperty("os.name").equals("Linux")) {
+            s = Parser.parseNXN("src/resources" + taille + "x" + taille + (hard ? "hard" : "") + ".csv");
+        } else {
+            s = Parser.parseNXN("Sudoku\\src\\resources\\" + taille + "x" + taille + (hard ? "hard" : "") + ".csv");
+        }
         System.out.println(s.basicToString());
         System.out.println("Solving...\n\n\n");
         long time = System.currentTimeMillis();
